7440aa95eefc86b8b6bab4f7a67ab9e35826f86e
Valery Gurov <va1ery@ya.ru>
2020-04-01 14:12:59 +0300

    Добавьте файлы проекта.

---
+++ DigitalLibraryApplication/Models/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalLibraryApplication.Models
+{
+    public class Publisher
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
