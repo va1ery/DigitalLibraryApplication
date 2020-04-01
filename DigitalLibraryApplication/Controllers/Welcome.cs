08d20cb98e4c925fa745fae3a69d446bc9bc9e82
Valery Gurov <va1ery@ya.ru>
2020-04-01 17:38:05 +0300

    Integration test works

---
+++ DigitalLibraryApplication/Controllers/Welcome.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalLibraryApplication.Controllers
+{
+    public class Welcome : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "Welcome to Digital Library!";
+        }
+        public string Description()
+        {
+            return "This is the application for managing digital books!";
+        }
+    }
+}
